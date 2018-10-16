efcdd137b5eef48bf51ae35d9f731e4ae4536649
vijay <vvsharma36@gmail.com>
2018-10-16 08:52:57 +0530

    ASSIGNMENT 3

---
+++ vv.py
@@ -0,0 +1,20 @@
+#first assignment to print something
+print('vijay')
+print(3+4)
+# add string
+print('hello' + 'world')
+# print the number of user defined variable
+number=input("enter the number")
+print(number)
+# print lets get started
+print("LETS GET STARTED")
+#print the value using placeholder
+name="vijay"
+age=24
+print('%s is %d years old' %(name,age))
+# print the area of circle
+radius=float(input("enter the radius"))
+PI=3.14
+area=PI*radius*radius
+print("area of the circle is",area)
+
