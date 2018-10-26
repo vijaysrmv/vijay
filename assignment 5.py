af685d2ffec5100e109d8086222807d37cf0edb1
vijay <vvsharma36@gmail.com>
2018-10-26 19:18:31 +0530

    ASSIGNMENT8

---
+++ assignment 5.py
@@ -0,0 +1,10 @@
+# user defined dictionary
+a=input("enter the key")
+b=input("enter the value of key")
+dect={a:b}
+print(dect)
+print(dect[a])
+# nested dictionary
+dict={'vijay':{'maths':90,'history':98},'ashutosh':{'maths':96,'history':87}}
+print(dict['vijay']['maths'])
+print(dict['ashutosh']['history'])
