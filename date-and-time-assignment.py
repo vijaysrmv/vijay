e51ef10f524bf500531afe349e71e6098cfa99a5
vijay <vvsharma36@gmail.com>
2018-11-02 07:49:05 +0530

    DATE AND TIME ASSIGNMENT

---
+++ date-and-time-assignment.py
@@ -0,0 +1,15 @@
+# print the today day
+import datetime
+from datetime import date
+d=datetime.datetime.now()
+print(datetime.datetime.now())
+print(d.day)
+# playing the toutube video using webbrowser
+import webbrowser
+webbrowser.open("https://www.youtube.com/watch?v=z476zI-Oajk")
+# change the multiple file name sing import
+import os
+os.rename("vijay.jpg","vijay.txt")
+# muje multiple file ka name rename karna nahi aaya
+# can you please tell me
+# as soon as possible
